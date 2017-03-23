57ace9809f6d85e2a5baae21639b52135865855b
Jason Brown and Xia Amendolara <xia@>
2017-03-23 09:27:54 -0700

    fix syntax errors

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('blog', params.blog_id);
-  }
+  },
   actions: {
     destroyBlog(blog) {
       blog.destroyRecord();
